423842c2d16db07d59a36091fcec8348af236388
Andrew Borgman <ndrwsbrgmn@gmail.com>
2014-08-29 12:37:27 -0400

    add in test data sets

--- data/data-gen.R
+++ data/data-gen.R
@@ -2,11 +2,34 @@
 # andrew borgman
 # varibbc
 # script to create some dummy data for students to read into r
+library(ggplot2)
+library(dplyr)
+library(tidyr)
 
+set.seed(030289)
 
 # build up an experimental data set
-treatment = rep(c("Control", "Treat1", "Treat2", "Combination"), each = 12)
-genotype = rep(c("WT/WT", "WT/KO", "KO/KO"), each = 4, length.out = length(treatment))
-vals =
-  
-xx = expand.grid(treatment = c("Control", "Treat1", "Treat2", "Combination"), genotype = c("WT/WT", "WT/KO", "KO/KO"))
+reps = 15
+dat = data.frame(
+  id = 1:reps, 
+  treatment = rep(c("Control", "Treat 1", "Treat 2", "Combination"), each = reps),
+  vals = c(
+    rnorm(reps, mean = 10, sd = 1),
+    rnorm(reps, mean = 8, sd = 1),
+    rnorm(reps, mean = 6, sd = 1),
+    rnorm(reps, mean = 4, sd = 1)    
+  )
+)
+
+ggplot(dat, aes(x = treatment, y = vals)) + geom_point(position = "jitter")
+
+wide = dat %>% spread(id, vals)
+
+write.table(dat, 'test_data_long.tsv', sep = "\t", col.names = T, row.names = F, quote = F)
+write.csv(dat, 'test_data_long.csv', row.names = F)
+write.table(dat, 'test_data_long.txt', col.names = T, row.names = F, quote = F)
+
+write.table(wide, 'test_data_wide.tsv', sep = "\t", col.names = T, row.names = F, quote = F)
+write.csv(wide, 'test_data_wide.csv', row.names = F)
+write.table(wide, 'test_data_wide.txt', col.names = T, row.names = F, quote = F)
+
